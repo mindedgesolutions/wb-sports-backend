255925e59768da3cac4a59da0c430c5956df4a7b
Souvik Nag <s.nag26@gmail.com>
2025-03-26 16:45:12 +0530

    news

--- database/migrations/2025_03_26_142119_create_districts_table.php
+++ database/migrations/2025_03_26_142119_create_districts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('districts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('district_code')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
