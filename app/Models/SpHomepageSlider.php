9813c46c91480435d5665611e38bb9a4113b8ec6
Souvik Nag <s.nag26@gmail.com>
2025-04-17 17:52:59 +0530

    news

---
+++ app/Models/SpHomepageSlider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SpHomepageSlider extends Model
+{
+    protected $fillable = [
+        'image_path',
+        'is_active',
+    ];
+}
